f8ff423486275d9fb0e584c71b195584e7a4522b
Andrew Vlahovich <andrewvlahovich@gmail.com>
2025-12-15 13:17:21 -0800

    fix: semi colons after functions

--- src/built_ins.c
+++ src/built_ins.c
@@ -7,8 +7,8 @@ char* built_ins[] = {
 // EXAMPLE: if only one built-in, that is ONE pointer, so 8 bytes. 8 / 8 = 1
 int num_built_ins() {
 	return sizeof(built_ins) / sizeof(char*);
-}
+};
 
 int kpa_exit(char** args) {
 	return 0;
-}
+};
