3cffd01e0d71e97c8a2a472ee27654f54c6ba2be
Andrew Vlahovich <andrewvlahovich@gmail.com>
2025-12-16 13:19:46 -0800

    feat: cd builtin func added

--- src/builtins.c
+++ src/builtins.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 
 int kpa_exit(char**);
+int kpa_cd(char**);
 
 char* builtins_strs[] = {
-	"exit"
+	"exit",
+	"cd"
 };
 
 int num_builtins() {
@@ -12,8 +14,22 @@ int num_builtins() {
 }
 
 int (*built_ins[])(char**) = {
-	&kpa_exit
+	&kpa_exit,
+	&kpa_cd
 };
+
+int kpa_cd(char** args) {
+	// Check for the directory in args[1]
+	if (args[1] == NULL) {
+		fprintf(stderr, "expected directory missing\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+				perror("koopa");
+		}
+	}
+
+	return 1;
+}
 
 int kpa_exit(char**) {
 	return 1;
