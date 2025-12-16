7f634588ebaeef4cf4776e084346bf874d4f1b1b
Andrew Vlahovich <andrewvlahovich@gmail.com>
2025-12-16 13:06:04 -0800

    edit: launch/execute functions

--- src/execute.c
+++ src/execute.c
@@ -3,8 +3,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
-#include "built_ins.h"
+#include "builtins.h"
 
 // Initializes process specified by the first arg, returns 1 when process terminates.
 int launch(char** args) {
@@ -16,11 +17,6 @@ int launch(char** args) {
 
 	if (pid == 0) {
 		// Child process
-
-		// Check for no commands given, should exit success rather than seg fault
-		if (args[0] == NULL) {
-			exit(EXIT_SUCCESS);
-		}
 
 		if (execvp(args[0], args) == -1) {
 			perror("execvp");
@@ -47,16 +43,19 @@ int launch(char** args) {
 	return 1;
 }
 
-
 int execute(char** args) {
-	int n = num_built_ins();
-
-	for (int i = 0; i < n; i++) {
-		if (strcmp(args[0], built_ins[i]) == 0) {
-			return (*built_in_funcs[i](args);
-		}
+	if (args[0] == NULL) {
+		return 1;      
 	}
+	int i;
+	int n = num_builtins();
 
-	// If not a built-in, call launch and return status
+	for (i = 0; i < n; i++) {
+		if (strcmp(args[0], builtins_strs[i]) == 0) {
+			return (*built_ins[i])(args);
+		}
+	}	
+	
 	return launch(args);
 }
+
