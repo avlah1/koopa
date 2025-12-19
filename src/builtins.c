139c01047936b294d17b187cc952ce148ec2519b
Andrew Vlahovich <andrewvlahovich@gmail.com>
2025-12-19 11:55:55 -0800

    edit: error message update in cd

--- src/builtins.c
+++ src/builtins.c
@@ -21,7 +21,7 @@ int (*built_ins[])(char**) = {
 int kpa_cd(char** args) {
 	// Check for the directory in args[1]
 	if (args[1] == NULL) {
-		fprintf(stderr, "expected directory missing\n");
+		fprintf(stderr, "error: expected directory for cd\n");
 	} else {
 		if (chdir(args[1]) != 0) {
 				perror("koopa");
