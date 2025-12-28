6483f9145bad9dd6d5c5581a9df296a299f26724
Andrew Vlahovich <andrewvlahovich@gmail.com>
2025-12-28 13:23:05 -0800

    feat: custom input parser built

--- src/input_handler.c
+++ src/input_handler.c
@@ -2,16 +2,95 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "colors.h"
 
 #define BUFSIZE 128
 
 // Buffer size for token buffer
 #define TOK_BUFSIZE 64
 
+#define TOKEN_SIZE 32
 // Delimiters for parse_line function
 #define TOK_DELIMS " \n\t" 
 
+char* get_token(char* str, char* delimeters) {
+	
+	// Static storage for subsequent calls to get_token
+	static char* next_token_start = NULL;
+	
+	size_t token_size = TOKEN_SIZE;
 
+	// The token buffer. Characters from str will be transferred here.
+	char* token_start = malloc(token_size * sizeof(char));
+
+	if (!token_start) {
+		fprintf(stderr, ERROR"allocator fail while get_token\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Now, input string
+	if (str != NULL) {
+		// First call to get_token
+		next_token_start = str;
+	} else {
+		// Subsequent calls to get_token
+		str = next_token_start;
+	}
+	
+	// If we are at the end of the string, there is nothing left to parse. Return null.
+	if (str == NULL || *str == '\0') {
+		return NULL;
+	}
+	
+	// Eat leading delimiters
+	while (*str != '\0' && strchr(delimeters, *str) != NULL) {
+		str++;
+	}
+	
+	// Return null if we nothing but delimters are left
+	if (*str == '\0') {
+		next_token_start = NULL;
+		return NULL;
+	}
+	
+	// Flag to keep track of whether or not we are in the middle quotes
+	int quoted = 0;
+
+	// Set the token pointer to the beginning of the buffer
+	int position = 0;
+	
+	while(*str != '\0') {
+		// If quotes, toggle quoted flag and skip this character
+		if (*str == '"') {
+			quoted = !quoted;
+			str++;
+		// If we aren't in the middle of quotes and we are currently looking at a delimiter, break.
+		} else if (!quoted && strchr(delimeters, *str) != NULL) {
+			break;
+		// Otherwise, move the current character into the buffer, increasing both the current buffer position and pointer to str
+		} else {
+			token_start[position] = *str;
+			position++;
+			str++;
+		}
+	}
+
+	if (quoted) {
+		fprintf(stderr, ERROR "argument missing balanced quotes\n");
+		free(token_start);
+		exit(EXIT_SUCCESS);
+	}
+
+	if (*str != '\0') {
+		*str = '\0';
+		next_token_start = str + 1;
+	} else {
+		next_token_start = NULL;
+	}
+	
+	return token_start;
+
+}
 // Tokenizes given line using the delimiters macro. 
 char** parse_line(char* line) {
 	int buffer_size = TOK_BUFSIZE;
@@ -26,8 +105,9 @@ char** parse_line(char* line) {
 	}
 	
 	// Get token and add to args array
-	token = strtok(line, TOK_DELIMS);
+	token = get_token(line, TOK_DELIMS);
 	while (token != NULL) {
+		printf("token: %s\n", token);
 		tokens[position] = token;
 		position++;
 
@@ -40,9 +120,8 @@ char** parse_line(char* line) {
 				exit(EXIT_FAILURE);
 			}
 		}
-                
 		// Continue parsing starting from where we left off
-		token = strtok(NULL, TOK_DELIMS);
+		token = get_token(NULL, TOK_DELIMS);
 	}
         
 	// Set the final character to null and return the array
@@ -65,7 +144,6 @@ char* read_line() {
 	
 	while (1) {
 		c = getchar();
-	        
 		// Detect newline character, setting character to null terminator, otherwise set to current character
 		if (c == '\n') {
 			buffer[position] = '\0';
