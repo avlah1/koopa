63f2b2450da32639bb23c701cc1e454de31631b3
Andrew Vlahovich <andrewvlahovich@gmail.com>
2025-12-29 14:50:16 -0800

    feat: input redirection functionality added

--- src/redirect.c
+++ src/redirect.c
@@ -7,9 +7,6 @@
 
 #include "colors.h"
 
-// Global flags with options for open syscall
-int open_flags = O_CREAT | O_WRONLY | O_TRUNC;
-
 // Macro function for syscall error detection
 #define SYS_ERR_CHECK(X) do { \
 	int retval = (X); \
@@ -19,37 +16,19 @@ int open_flags = O_CREAT | O_WRONLY | O_TRUNC;
 	} \
 } while(0)
 
-// Finds either > or >> and updates global accordingly. Splits the args buffer to separate redirection destination from the rest of the arguments. Returns the file destination if either symbol was found, returns NULL otherwise.
-char** get_redirect_dest(char** args) {
-	int i = 0;
-	int redirect_flag = 0;
 
-	while (args[i] != NULL) {
-		if (strcmp(args[i], ">") == 0) { 
-			redirect_flag = 1;
-		} else if (strcmp(args[i], ">>") == 0) {
-			redirect_flag = 1;
-			open_flags = O_CREAT | O_WRONLY | O_APPEND;
-		}
-		
-		if (redirect_flag) {
-			args[i] = NULL;
-			return &args[i+1];
-		}
 
-		i++;
-	}
-	return NULL;
-}
-
-//output redirection
+// Determines if clobbering or appending and updates flags accordingly. Opens file, redirect stdout to the file. Clears the redirect token by setting position to NULL. 
 void redirect_out(char** args) {
+	
+	int open_flags = O_CREAT | O_WRONLY;
+
 	if (strcmp(args[0], ">>") == 0) {
-		open_flags = O_CREAT | O_WRONLY | O_APPEND;
+		open_flags |= O_APPEND;
 	} else {
-		open_flags = O_CREAT | O_WRONLY | O_TRUNC; 
+		open_flags |= O_TRUNC; 
 	}
-	args[0] = NULL;
+
 	int file_desc = open(args[1], open_flags, 0644);
 
 	SYS_ERR_CHECK(file_desc);
@@ -57,44 +36,38 @@ void redirect_out(char** args) {
 	SYS_ERR_CHECK(dup2(file_desc, 1));
 
 	SYS_ERR_CHECK(close(file_desc));
+
+	args[0] = NULL;
 }
 
-
+// Opens file to read from, redirect stdin to file. Clears the redirect token by setting position to NULL.
 void redirect_in(char** args) {
-}
-// Redirection entry point. Calls redirect dest function, and if a file was not returned, do nothing.  Otherise, we open a file, redirect stdout to new file, and close the new file.
-void find_redirection(char** args) {
+	
+	int file_desc = open(args[1], O_RDONLY);
 
-	// Idea: take the loop that is on lines 27-40 and move it down here. This loop will branch off into 4 different functions, one for each of the 
-	// redirection symbols: < > >> |
-	//
-	//
+	SYS_ERR_CHECK(file_desc);
+
+	SYS_ERR_CHECK(dup2(file_desc, STDIN_FILENO));
+
+	SYS_ERR_CHECK(close(file_desc));
+
+	args[0] = NULL;
+}
+
+// Redirection entrypoint. Loops through args, if a redirection token is found, calls the appropriate helper. Otherwise, return to caller.
+void find_redirection(char** args) {
 	
 	int i = 0;
+
 	while (args[i] != NULL) {
 		if ((strcmp(args[i], ">") == 0) || strcmp(args[i], ">>") == 0) {
 			redirect_out(&args[i]);	
-		} else if (strcmp(args[i], "<<") == 0) {
-			// input redirect
+		} else if (strcmp(args[i], "<") == 0) {
 			redirect_in(&args[i]);
 		}
-		// else, do nothing
 		i++;
 	}	
-/*	
-	char** filename = get_redirect_dest(args);
-        
-	if (filename) {
-		// ALL THE SYSCALLS
-		int file_desc = open(filename[0], open_flags, 0644);
 
-		SYS_ERR_CHECK(file_desc);
-
-		SYS_ERR_CHECK(dup2(file_desc, 1));
-
-		SYS_ERR_CHECK(close(file_desc));
-	}
-
-*/
+	return;
 }
 
