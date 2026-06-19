87f3b4389d257a79f384487121fff6793d9aaf60
Andrew Vlahovich <avlaho@cs.washington.edu>
2026-06-19 14:56:33 -0700

    edit: added 'Koopa:' to RED

--- include/Colors.h
+++ include/Colors.h
@@ -2,7 +2,7 @@
 #define COLORS_H
 
 #define ERROR "\033[31mError: \033[0m"
-#define COLOR_RED "\033[31m"
+#define COLOR_RED "\033[31mKoopa:"
 #define COLOR_GREEN "\033[3;32m"
 #define COLOR_BLUE "\033[34m"
 #define COLOR_END "\033[0m"
